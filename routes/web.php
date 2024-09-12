fd8a595a5149728241fe086b96b80d98a648a33d
florianmg <mesagouiffes@gmail.com>
2024-09-12 19:57:54 +0200

    introduce routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('/blog')->name('blog.')->group(function () {
+    Route::get('/', function (Request $request) {
+        return [
+            'title' => 'Blog',
+            'content' => 'This is a blog page',
+            'page' => $request->input('page'),
+            'route' => route('blog.show', [
+                'slug' => 'my-first-post',
+                'id' => 1,
+            ]),
+        ];
+    })->name('index');
+    Route::get('/{slug}-{id}', function (string $slug, string $id) {
+        return [
+            'title' => 'Blog',
+            'slug' => $slug,
+            'id' => $id,
+        ];
+    })->where([
+        'id' => '[0-9]+',
+        'slug' => '[a-z0-9-]+',
+    ])->name('show');
+});
+
+
